f3438140c61ba05fe3cee4df0922df164d6a368a
pooraniyasho <poorani.yasho@gmail.com>
2026-07-09 19:13:13 +0530

    jenkin credentials test

--- fixtures/loginfixture.js
+++ fixtures/loginfixture.js
@@ -1,13 +1,13 @@
 import{test as base} from '@playwright/test';
 import {LoginPage} from  '../pages/login.js';
 import data from '../test-data/login.json';
-
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
 export let test= base.extend({
     loginPage: async({page},use)=>{
         const sign= new LoginPage(page);
         await sign.launch(data.url);
-        const username = process.env.USERNAME;
-         const password = process.env.PASSWORD;
+        
 
     if (!username || !password) {
       throw new Error('USERNAME or PASSWORD is missing from Jenkins credentials');
