d2b92ab3e98ea70a75471820743e1c2f2cb79c57
pooraniyasho <poorani.yasho@gmail.com>
2026-07-09 19:35:23 +0530

    fixing local credetials  with .env file

--- fixtures/loginfixture.js
+++ fixtures/loginfixture.js
@@ -4,8 +4,8 @@ import data from '../test-data/login.json';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: './credentials.env' });
-const username = process.env.USERNAME;
-const password = process.env.PASSWORD;
+const username = process.env.APP_USERNAME;
+const password = process.env.APP_PASSWORD;
 export let test= base.extend({
     loginPage: async({page},use)=>{
         const sign= new LoginPage(page);
@@ -15,7 +15,7 @@ export let test= base.extend({
     if (!username || !password) {
       throw new Error('USERNAME or PASSWORD is missing from Jenkins credentials');
     }
-        await sign.logindetail(username,);password
+        await sign.logindetail(username,password)
         await use(page)
         await sign.signout(data.url)
     }
